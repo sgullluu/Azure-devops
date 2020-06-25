97f87febd905f3c326a3c4ce74ef618fec4596a8
sgullluu <43342312+sgullluu@users.noreply.github.com>
2020-06-26 01:06:34 +0530

    Update AgentInstallWindows.ps1

--- AgentInstallWindows.ps1
+++ AgentInstallWindows.ps1
@@ -1,10 +1,9 @@
-cd C:\Users\AzureUser
-mkdir azagent
-mkdir sourabh
-cd C:\Users\AzureUser\azagent
+cd C:\
+mkdir azagent12
+cd C:\azagent12
 invoke-WebRequest -Uri https://agent09090.s3.ap-south-1.amazonaws.com/vsts-agent-win-x64-2.170.1.zip -OutFile vsts-agent-win-x64-2.170.1.zip
-Expand-Archive -LiteralPath C:\Users\AzureUser\azagent\vsts-agent-win-x64-2.170.1.zip -DestinationPath C:\azagent\vsts-agent-win-x64-2.170.1 
-Start-Sleep -s 60
+Expand-Archive -LiteralPath C:\azagent12\vsts-agent-win-x64-2.170.1.zip -DestinationPath C:\azagent12\vsts-agent-win-x64-2.170.1
+cd C:\azagent12\vsts-agent-win-x64-2.170.1
 .\config.cmd --unattended --url "https://dev.azure.com/SimplifyGroup" --auth pat --token "wacar63k5zhgfv3p3fsatew2meolskh4e3t5azzogbfge3hx57rq" --pool "ZF" --agent "windows" --runAsService
 Stop-Transcript
 exit 0
